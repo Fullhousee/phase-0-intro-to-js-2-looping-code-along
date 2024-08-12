05aceb95a04bf36a2a87ad8d95c94fd1b2be69cf
Andrew Fuller <fuller.andrew55@gmail.com>
2024-08-12 15:32:24 -0500

    Got those bad boys runnin

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solutions in this file
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+
+function writeCards(arr, event){
+    const messages = []
+    for (let i = 0; i < arr.length; i++){
+        const greetingMessage =`Thank you, ${arr[i]}, for the wonderful ${event} gift!`
+        messages.push(greetingMessage)
+    }
+    return messages
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+
+function countDown(num){
+    while (num > -1){
+      console.log(num--)
+    }
+
+}
+
+countDown(20)
